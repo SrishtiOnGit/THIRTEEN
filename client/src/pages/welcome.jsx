dd32190978ffa1448339f00f3b763579ee621223
Srishti Srivastava <srishti.school2010@gmail.com>
2026-09-21 22:09:07 +0530

    cleared the pre-defined code by vite..

---
+++ client/src/pages/welcome.jsx
@@ -0,0 +1,11 @@
+const Welcome = () => {
+  return (
+    <div>
+      <h1>THE THIRTEEN</h1>
+      <p>Some flames are better left unlit.</p>
+      <button>Begin</button>
+    </div>
+  );
+};
+
+export default Welcome;
