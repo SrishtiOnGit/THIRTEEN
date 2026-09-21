62074cc40fc1076b4a66aac3135de92628ac73e5
Srishti Srivastava <srishti.school2010@gmail.com>
2026-09-21 22:43:51 +0530

    feat : created the welcome page with some images and text..

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "./pages/welcome.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  );
+};
+
+export default App;
--- client/src/pages/welcome.jsx
+++ client/src/pages/welcome.jsx
@@ -1,9 +1,15 @@
+import himg from "../assets/image.png";
+import halloween from "../assets/halloween-g.jpg";
+import "./welcome.css";
+
 const Welcome = () => {
   return (
-    <div>
+    <div className="welcome">
+      <img src={himg} alt="IMG" className="himg" />
+      <img src={halloween} alt="HIMG" className="halloweenimg" />
       <h1>THE THIRTEEN</h1>
-      <p>Some flames are better left unlit.</p>
-      <button>Begin</button>
+      <p className="p1">Some flames are better left unlit.</p>
+      <button className="button-h">Begin</button>
     </div>
   );
 };
